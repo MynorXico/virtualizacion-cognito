bfbfd6f9ec68f32c13bd7ba225564915e61c1233
Mynor Xico <xic14659@uvg.edu.gt>
2020-07-03 19:29:29 -0600

    Enable CORS

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
-'use strict';
-const dotenv = require('dotenv');
+"use strict";
+const dotenv = require("dotenv");
 
 dotenv.config();
-var path = require('path');
-var http = require('http');
+var path = require("path");
+var http = require("http");
 
-var oas3Tools = require('oas3-tools');
+var oas3Tools = require("oas3-tools");
 var serverPort = process.env.PORT;
 
 // swaggerRouter configuration
 var options = {
-    controllers: path.join(__dirname, './controllers')
+  controllers: path.join(__dirname, "./controllers"),
 };
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+var expressAppConfig = oas3Tools.expressAppConfig(
+  path.join(__dirname, "api/openapi.yaml"),
+  options
+);
 expressAppConfig.addValidator();
 var app = expressAppConfig.getApp();
 
-// Initialize the Swagger middleware
-http.createServer(app).listen(serverPort, "0.0.0.0", function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
 });
 
+// Initialize the Swagger middleware
+http.createServer(app).listen(serverPort, "0.0.0.0", function () {
+  console.log(
+    "Your server is listening on port %d (http://localhost:%d)",
+    serverPort,
+    serverPort
+  );
+  console.log(
+    "Swagger-ui is available on http://localhost:%d/docs",
+    serverPort
+  );
+});
