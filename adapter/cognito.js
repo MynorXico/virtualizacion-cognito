a199656ca2f7b31bd527a995431857e317b558c1
Mynor Xico <xic14659@uvg.edu.gt>
2020-07-04 00:43:42 -0600

    Handles no jwt as token

--- adapter/cognito.js
+++ adapter/cognito.js
@@ -1,61 +1,68 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const jwkToPem = require('jwk-to-pem');
-const User = require('../models/user').User;
-const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const jwt_decode = require('jwt-decode');
+const jwkToPem = require("jwk-to-pem");
+const User = require("../models/user").User;
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const jwt_decode = require("jwt-decode");
 
 dotenv.config();
 
-
 exports.validate = async (token) => {
-    return new Promise(async(resolve, reject) => {
-        var decoded = (jwt_decode(token));
-        
-        const cognito_url = `${decoded.iss}/.well-known/jwks.json`;
+  return new Promise(async (resolve, reject) => {
+    try {
+      var decoded = jwt_decode(token);
+    } catch (err) {
+      reject({
+        error: true,
+        message: "Not a valid JWT token",
+      });
+    }
 
-        console.log("Decoded: ", decoded);
-        await axios.get(cognito_url)
-            .then(({status, data}) => {
-                pems = {};
-                var keys = data.keys;
-                for(var i = 0; i < keys.length; i++){
-                    var key_id = keys[i].kid;
-                    var modulus = keys[i].n;
-                    var exponent = keys[i].e;
-                    var key_type = keys[i].kty;
-                    var jwk = {kty: key_type, n: modulus, e: exponent};
-                    var pem = jwkToPem(jwk);
-                    pems[key_id] = pem;
-                }
+    const cognito_url = `${decoded.iss}/.well-known/jwks.json`;
 
-                var decodedJwt = jwt.decode(token, {complete: true});
-            
-                if(!decodedJwt){
-                    reject({
-                        "error": true,
-                        "message": "Not a valid JWT token"
-                    });
-                }
-                
-                var kid = decodedJwt.header.kid;
-                var pem = pems[kid];
-                if(!pem){
-                    reject({
-                        "error": true,
-                        "message": "Not a valid JWT token"
-                    });
-                }
+    console.log("Decoded: ", decoded);
+    await axios
+      .get(cognito_url)
+      .then(({ status, data }) => {
+        pems = {};
+        var keys = data.keys;
+        for (var i = 0; i < keys.length; i++) {
+          var key_id = keys[i].kid;
+          var modulus = keys[i].n;
+          var exponent = keys[i].e;
+          var key_type = keys[i].kty;
+          var jwk = { kty: key_type, n: modulus, e: exponent };
+          var pem = jwkToPem(jwk);
+          pems[key_id] = pem;
+        }
 
-                jwt.verify(token, pem, function(err, payload) {
-                    if(err) reject({"success": false, "message": "Not a valid JWT token"})
-                    var user = new User(payload);
-                    resolve(user);
-                })
-            })
-            .catch((e) => {
-                reject(e);
-            });
-    });
-}
+        var decodedJwt = jwt.decode(token, { complete: true });
+
+        if (!decodedJwt) {
+          reject({
+            error: true,
+            message: "Not a valid JWT token",
+          });
+        }
+
+        var kid = decodedJwt.header.kid;
+        var pem = pems[kid];
+        if (!pem) {
+          reject({
+            error: true,
+            message: "Not a valid JWT token",
+          });
+        }
+
+        jwt.verify(token, pem, function (err, payload) {
+          if (err) reject({ success: false, message: "Not a valid JWT token" });
+          var user = new User(payload);
+          resolve(user);
+        });
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+};
